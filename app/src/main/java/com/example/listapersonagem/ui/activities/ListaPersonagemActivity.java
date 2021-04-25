06465fe64d11c9768a53fb560dda52862834dec2
Felipe Laureano <felipe.laureano@hotmail.com>
2021-04-25 16:47:33 -0300

    Attualizaçao Aula 25-03

--- app/src/main/java/com/example/listapersonagem/ui/activities/ListaPersonagemActivity.java
+++ app/src/main/java/com/example/listapersonagem/ui/activities/ListaPersonagemActivity.java
@@ -3,6 +3,7 @@ package com.example.listapersonagem.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,17 +11,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.listapersonagem.R;
 import com.example.listapersonagem.dao.PersonagemDAO;
+import com.example.listapersonagem.model.Personagem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class ListaPersonagemActivity extends AppCompatActivity {
 
-    @Override
+    @Override //sobrescreve
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_personagem);
-
-        PersonagemDAO dao = new PersonagemDAO();
-
         setTitle("Lista de Personagens");
 
         FloatingActionButton botaoNovoPersonagem = findViewById(R.id.fab_novo_personagem);//criação de vinculo com o botão
@@ -32,10 +33,6 @@ public class ListaPersonagemActivity extends AppCompatActivity {
         });
 
         //List<String> personagem = new ArrayList<>(Arrays.asList("Alex", "Ken", "Ryu"));
-
-        ListView listaDePersonagens = findViewById(R.id.lista_personagem);
-        listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
-
 
 
         //FAZER MANUALMENTE:
@@ -49,4 +46,22 @@ public class ListaPersonagemActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        PersonagemDAO dao = new PersonagemDAO();
+        ListView listaDePersonagens = findViewById(R.id.lista_personagem);
+        List<Personagem> personagens = dao.todos();
+        listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, personagens));
+        listaDePersonagens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Personagem personagemEscolhido = personagens.get(posicao);
+                //Log.i( tag: "personagem", msg: "" + personagemEscolhido); //Vizualizar no LogCat
+                Intent vaiParaFormulario = new Intent(ListaPersonagemActivity.this, FormularioPersonagemActivity.class);
+                startActivity(vaiParaFormulario);
+            }
+        });
+    }
 }
